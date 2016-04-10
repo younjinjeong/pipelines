d629cc00bb87f642cf2dd8d1a5cdfbeca3124032
Younjin Jeong <jeongy4@Younjin-Jeongs-MacBook-Pro.local>
2016-04-10 17:24:41 +0900

    Adding Git install- remove other packages

--- blog/yjeong_blog_run_tests.sh
+++ blog/yjeong_blog_run_tests.sh
@@ -10,8 +10,7 @@ set -e
 
 printenv
 uname -a 
-apk add --update git python python-dev py-pip build-base
-git -h
+apk add --update git 
 go get -v github.com/spf13/hugo
 go get -u -v github.com/spf13/hugo 
 cd ./yjeong-blog-git
